eaa4185946cb9e3e3279c4c4c2c5faa5a3d1c963
patricia <patriciapavia236@gmail.com>
2020-07-06 22:09:21 +1000

    fixed

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="container text-center">
-      <h1>What's On Hot News AU</h1>
+      <h1>What's On Hot News </h1>
     </div>
   );
 }
